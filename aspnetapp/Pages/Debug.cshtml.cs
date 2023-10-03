277c0ca7c7f146ab9201bab7c1e27845fab1a966
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2023-10-03 09:50:04 -0500

    Rename DBUrl Page to Debug
    Only show DATABASE_URL if SHOW_DATABASE_URL is TRUE

--- aspnetapp/Pages/Debug.cshtml.cs
+++ aspnetapp/Pages/Debug.cshtml.cs
@@ -3,11 +3,11 @@ using Microsoft.Extensions.Logging;
 
 namespace aspnetapp.Pages
 {
-    public class DBUrlModel : PageModel
+    public class DebugModel : PageModel
     {
-        private readonly ILogger<DBUrlModel> _logger;
+        private readonly ILogger<DebugModel> _logger;
 
-        public DBUrlModel(ILogger<DBUrlModel> logger)
+        public DebugModel(ILogger<DebugModel> logger)
         {
             _logger = logger;
         }
