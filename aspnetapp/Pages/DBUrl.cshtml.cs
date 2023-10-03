bf802115e3c639744d3e89aef85aad84da4ddf9e
Ross W. Thanscheidt <ross.thanscheidt@hobbylobby.com>
2023-10-03 09:38:36 -0500

    Add DBUrl page
    (temporary, for debugging)

---
+++ aspnetapp/Pages/DBUrl.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetapp.Pages
+{
+    public class DBUrlModel : PageModel
+    {
+        private readonly ILogger<DBUrlModel> _logger;
+
+        public DBUrlModel(ILogger<DBUrlModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
